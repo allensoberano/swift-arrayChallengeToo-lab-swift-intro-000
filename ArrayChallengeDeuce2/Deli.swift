7078047429bebdb65cab49304ed75cc20d59c50e
Allen Soberano <allensoberano@gmail.com>
2016-11-03 10:06:03 -0700

    done

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -10,30 +10,56 @@
 
 
 class Deli {
-    
+
     var line: [String] = []
-    
+
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
-        
+
+        // TODO: Implement this function.
+        switch name {
+        case "Billy Crystal", "Meg Ryan":
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        default:
+            line.append(name)
+            if line.count == 1 {
+                return "Welcome \(name), you're first in line!"
+            } else {
+                return "Welcome \(name), you're number \(line.count) in line."
+            }
+
+        }
     }
-    
-    
+
     // 2
     func nowServing() -> String {
-        
+
         // TODO: Implement this function.
+        if line.isEmpty {
+            return "There is no one to be served."
+        } else {
+            let name = line[0]
+            line.remove(at: 0)
+            return "Now serving \(name)!"
+        }
 
     }
-    
-    
+
     // 3
     func lineDescription() -> String {
-        
-        // TODO: Implement this function.
 
+        var returnString = "The line is:"
+
+        // TODO: Implement this function.
+        if line.isEmpty {
+            return "The line is currently empty."
+        } else {
+            for (index, name) in line.enumerated() {
+                returnString.append("\n\(index + 1). \(name)")
+            }
+            return returnString
+        }
     }
-    
+
 }
